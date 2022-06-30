499988d7e4652e107395c0477654b55607123ee4
mborreros <mlbsindia@gmail.com>
2022-06-30 13:19:53 -0400

    Added show action to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users, status: :ok
+  end
+
+  def show
+    user = User.find_by(id: params[:id])
+    if user
+      render json: user, status: :ok
+    else
+      render json: {error: "User not found"}, status: :not_found
+    end
+  end
+
 end
