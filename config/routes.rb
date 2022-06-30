b8dda803d87fda9220be4c830e271919e63bfe54
mborreros <mlbsindia@gmail.com>
2022-06-30 14:50:14 -0400

    Added Fallback Controller for routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
+  namespace :api do
+    #all backend routes go here
+    resources :users, only: [:index, :show]
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
