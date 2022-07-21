c4ec0062222e34446c41f93404f490d5c6ad531f
mborreros <mlbsindia@gmail.com>
2022-07-21 13:05:33 -0400

    corrected before_action method from cohort meet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,21 @@ class ApplicationController < ActionController::API
 
   include ActionController::Cookies
 
-  # before_action :authorized
+  before_action :authorized
 
+  # need to refactor this method here
   # def authorized
   #   return render json: {error: "User not authorized, please login to view content"}, status: :unauthorized
   #   unless session.include? :user_id
   #   end
   # end
+
+  # might ask you to refactor in the review utilizing the unless
+  def authorized
+    if session.include? :user_id
+    else
+      return render json: {error: "User not authorized, please login to view content"}, status: :unauthorized
+    end
+  end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-  # skip_before_action :authorized
-  # , only: :show
+  skip_before_action :authorized, only: :show
 
   def index
     users = User.all
